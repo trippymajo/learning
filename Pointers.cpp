4d708c1c7447d619013ec3ac02f40f4ba09ae331
elyadesu <elya.shafeeva@gmail.com>
2021-12-19 17:49:33 +0300

    C class constructor with pointer to A&B classes

--- Pointers.cpp
+++ Pointers.cpp
@@ -6,34 +6,63 @@ class A {
 public:
     int inta = 1;
     int showb();
-    B* b;
+    B* ptrA2B;
 };
 
 class B {
 public:
     int intb = 2;
     void showa();
-    A* a;
+    A* ptrB2A;
+};
+
+class C {
+public:
+    C();
+    ~C();
+    A* ptrC2A;
+    B* ptrC2B;
+    int showab();
+};
+
+C::C()
+{
+    ptrC2A = new A;
+    ptrC2B = new B;
+    cout << "Pointers for class C created\n";
+}
+
+C::~C()
+{
+    cout << "Destructed\n";
+}
+int C::showab()
+{
+    cout << "\nA class & B class out :" << ptrC2A->inta << " & " << ptrC2B->intb << "\n";
 };
 
 int A::showb()
 {
-    cout << "\nB class out :" << b->intb << "\n";
+    cout << "\nB class out :" << ptrA2B->intb << "\n";
 };
 
 void B::showa()
 {
-    cout << "\nA class out :" << a->inta << "\n";
+    cout << "\nA class out :" << ptrB2A->inta << "\n";
 };
+
 
 int main()
 {
     A a;
     B b;
+    C c;
 
-    a.b = &b;
-    b.a = &a;
+    a.ptrA2B = &b;
+    b.ptrB2A = &a;
+
     a.showb();
     b.showa();
+    c.showab();
     return 1;
 }
