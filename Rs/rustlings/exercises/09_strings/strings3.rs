7586e5f232ba741e083f0e8767af1d0af38b65e5
trippymajo <elyadesu@yandex.ru>
2025-12-23 21:32:58 +0200

    Rust book + excercises. Modules done

--- Rs/rustlings/exercises/09_strings/strings3.rs
+++ Rs/rustlings/exercises/09_strings/strings3.rs
@@ -1,13 +1,19 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    let mut str = String::with_capacity(input.len() + " world!".len());
+    str.push_str(input);
+    str.push_str(" world!");
+    str
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    input.replace("cars", "balloons")
 }
 
 fn main() {
