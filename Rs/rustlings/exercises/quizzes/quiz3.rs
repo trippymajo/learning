722a2f171e42fe21a420cc6d2535f784cc91f7d8
trippymajo <elyadesu@yandex.ru>
2026-03-08 14:29:09 +0200

    Rust. Iterators are next

--- Rs/rustlings/exercises/quizzes/quiz3.rs
+++ Rs/rustlings/exercises/quizzes/quiz3.rs
@@ -11,15 +11,21 @@
 // Make the necessary code changes in the struct `ReportCard` and the impl
 // block to support alphabetical report cards in addition to numerical ones.
 
+use core::fmt;
+
 // TODO: Adjust the struct as described above.
-struct ReportCard {
-    grade: f32,
+trait PrintReportCard {
+    fn print(&self) -> String;
+}
+
+struct ReportCard<T> {
+    grade: T,
     student_name: String,
     student_age: u8,
 }
 
 // TODO: Adjust the impl block as described above.
-impl ReportCard {
+impl <T:fmt::Display>PrintReportCard for ReportCard<T> {
     fn print(&self) -> String {
         format!(
             "{} ({}) - achieved a grade of {}",
